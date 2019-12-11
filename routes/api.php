ae7f5b54b8b31f9a0eea58dd54dced184f882edf
teevon <tegovona@gmail.com>
2019-12-11 08:34:55 +0100

    trying to fix post to get issue

--- routes/api.php
+++ routes/api.php
@@ -17,22 +17,22 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('guests', 'GuestController@index');
-Route::get('guests/create', 'GuestController@create');
-Route::post('guests', 'GuestController@store');
-Route::get('guests/{guest}', 'GuestController@show');
-Route::get('guests/{guest}/edit', 'GuestController@edit');
-Route::put('guests/{guest}', 'GuestController@update');
-Route::delete('guests/{guest}', 'GuestController@destroy');
+Route::get('/guests', 'GuestController@index');
+Route::get('/guests/create', 'GuestController@create');
+Route::post('/guests', 'GuestController@store');
+Route::get('/guests/{guest}', 'GuestController@show');
+Route::get('/guests/{guest}/edit', 'GuestController@edit');
+Route::put('/guests/{guest}', 'GuestController@update');
+Route::delete('/guests/{guest}', 'GuestController@destroy');
 
-Route::post('bookings/custom', 'BookingController@custom_listing');
+Route::post('/bookings/custom', 'BookingController@custom_listing');
 Route::post('/bookings', 'BookingController@store');
 
-Route::post('rooms', 'RoomController@index');
-Route::get('rooms/category', 'RoomController@category');
+Route::post('/rooms', 'RoomController@index');
+Route::get('/rooms/category', 'RoomController@category');
 
-Route::get('reservations/conflicts', 'ReservationController@date_conflict');
+Route::get('/reservations/conflicts', 'ReservationController@date_conflict');
 
-Route::post('payments', 'PaymentController@store');
+Route::post('/payments', 'PaymentController@store');
 
-Route::post('transactions', 'TransactionController@store');
+Route::post('/transactions', 'TransactionController@store');
